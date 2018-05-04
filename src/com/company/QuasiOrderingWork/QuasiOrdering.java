b89cfe0824217a39b68684ac986a7ed2dbb9f222
Jared Frank <jfrank1120@verizon.net>
2018-05-03 20:06:54 -0400

    Change for Quasi-Ordering - New method for checking subsetness (Credit Luke)

--- src/com/company/QuasiOrderingWork/QuasiOrdering.java
+++ src/com/company/QuasiOrderingWork/QuasiOrdering.java
@@ -4,22 +4,47 @@ import java.util.*;
 public class QuasiOrdering {
     public static HashSet<String> quasiSort(Binary_Collection inputData) {
         HashSet<String> non_quasi_strings = new HashSet<>();
-        HashSet<Integer> //TODO Create a sudo table to check is I is a subset of J
+        /*
+        int[] zero = {1 , 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+        int[] one = {3, 5, 7, 9, 11, 13, 15};
+        int[] two = {2, 3, 6, 7, 10, 11, 13, 15};
+        int[] three = {5, 7};
+        int[] four = {4, 5, 6, 7, 12, 13, 14, 15};
+        int[] five = {7, 13, 15};
+        int[] six = {7, 14, 15};
+        int[] seven = {15};
+        int[] eight = {9, 10, 11, 12, 13, 14, 15};
+        int[] nine = {11, 13, 15};
+        int[] ten = {11, 14, 15};
+        int[] eleven = {15};
+        int[] twelve = {13, 14, 15};
+        int[] thirteen = {15};
+        int[] fourteen = {15};
+        int[] fifteen = {15};
+        int[][] isASubsetOf = {zero, one, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve, thirteen, fourteen, fifteen};
+        */
         // Bit comparison
         for (int k = 0; k < inputData.binaryStrings.size() - 1; ++k) {
             boolean isSorted = true;
             // Loops through all strings
-            for (int i = 0,j = 0; (j + 1 < inputData.binaryStrings.get(k).length()); ++j, ++i) {
+            for (int i = 0,j = 0; (inputData.binaryStrings.get(i).length() - (j + 1) > 0); ++j, ++i) {
                 // Loops through characters within the string
-                    int bitI = Character.getNumericValue(inputData.binaryStrings.get(k).charAt(i));
-                    int bitJ = Character.getNumericValue(inputData.binaryStrings.get(k).charAt(j + 1));
-
-                    if (bitI < bitJ && i <= j) {
+                    int bitI = Character.getNumericValue(inputData.binaryStrings.get(k).charAt(inputData.binaryStrings.get(i).length() - (i)));
+                    int bitJ = Character.getNumericValue(inputData.binaryStrings.get(k).charAt(inputData.binaryStrings.get(i).length() - (j + 1)));
+                    boolean isASub = false;
+                    String binI = Integer.toBinaryString(i);
+                    String binJ = Integer.toBinaryString(j);
+                    for (int p = binJ.length(); p > 0; p--) {
+                        if (binI.charAt(p) > binJ.charAt(p)) {
+                            isASub = false;
+                        }
+                    }
+                    if (bitI < bitJ && !isASub) {
                         isSorted = false;
                         break;
                     }
                     if (!isSorted) {
-                        //System.out.println("Adding: " + inputData.binaryStrings.get(i));
+                        System.out.println("Adding: " + inputData.binaryStrings.get(i));
                         non_quasi_strings.add(inputData.binaryStrings.get(i));
                     }
                 }
